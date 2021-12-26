627672ce0fd8dd68f02ad75d232fc87ad3fd2408
Miron Lončarić <mironloncaric@debian-BULLSEYE-live-builder-AMD64>
2021-12-26 11:32:02 +0100

    Fixed several imports

--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,13 @@ import { SessionProvider } from "next-auth/react";
 import index from "./index.js";
 import "../components/global.css";
 import Layout from "../components/layout.js";
-import { TokenProvider } from "../components/tokencontext.js";
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider>
-      <TokenProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </TokenProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   );
 }
